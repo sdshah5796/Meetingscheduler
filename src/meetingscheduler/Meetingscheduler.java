24a4f95de4ecf7548c0216fbc388cc45be4ea4ee
Meet <Meet@Smit-PC>
2017-02-02 11:55:58 +0530

    Trial

--- src/meetingscheduler/Meetingscheduler.java
+++ src/meetingscheduler/Meetingscheduler.java
@@ -15,7 +15,7 @@ public class Meetingscheduler {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Fuck you");
         // TODO code application logic here
     }
     
